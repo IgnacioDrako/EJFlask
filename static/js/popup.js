2368e9984c15f30ca8e3ca076622258fc9cc1e4b
IgnacioDrako <ignaciodelgado079camacho@gmail.com>
2025-01-23 18:19:47 +0100

    Custom_Alert

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert('Necesitas permisos!');
+    Swal.fire({
+        title: 'Necesitas permisos CRACK!',
+        text: 'Esta página solo puede ser vista por administradores.',
+        imageUrl: '../static/img/cat.gif',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        confirmButtonText: 'Entendido',
+        background: '#111',
+    });
 });
